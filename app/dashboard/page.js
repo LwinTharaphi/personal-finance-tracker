41adc686b84e5804514d41f21bce5d3f1d0d6522
AeliaWin <u6540064@au.edu>
2024-09-23 18:42:27 +0700

    Added Dashboard and SideBar new Style

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,22 @@
 "use client"; // Client component
 
 import { useState, useEffect } from 'react';
-import { Container, Row, Col, Card, Table, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Card, Table, Alert, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from '../components/Sidebar';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Title, Tooltip, Legend);
 
 export default function Dashboard() {
   const [incomeList, setIncomeList] = useState([]);
   const [expenseList, setExpenseList] = useState([]);
   const [error, setError] = useState('');
+  const [incomeSelectedMonth, setIncomeSelectedMonth] = useState(new Date().getMonth());
+  const [expenseSelectedMonth, setExpenseSelectedMonth] = useState(new Date().getMonth());
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,7 +40,96 @@ export default function Dashboard() {
 
   const totalIncome = incomeList.reduce((total, income) => total + income.amount, 0);
   const totalExpenses = expenseList.reduce((total, expense) => total + expense.amount, 0);
-  const budget = totalIncome - totalExpenses;
+  const totalNetWork = totalIncome - totalExpenses;
+
+  // Function to aggregate monthly data
+  const getMonthlyData = (data) => {
+    const monthlyTotals = Array(12).fill(0);
+    data.forEach(item => {
+      const month = new Date(item.date).getMonth();
+      monthlyTotals[month] += item.amount;
+    });
+    return monthlyTotals;
+  };
+
+  const incomeByMonth = getMonthlyData(incomeList);
+  const expenseByMonth = getMonthlyData(expenseList);
+
+  const chartData = {
+    labels: [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ],
+    datasets: [
+      {
+        label: 'Income',
+        data: incomeByMonth,
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+      },
+      {
+        label: 'Expenses',
+        data: expenseByMonth,
+        backgroundColor: 'rgba(255, 99, 132, 0.6)',
+      },
+    ],
+  };
+
+  const filterByMonth = (data,month)=>{
+    return data.filter(item=> new Date(item.date).getMonth() === month);
+  };
+
+  const getCategoryData = (data,key) => {
+    return data.reduce((acc,item)=>{
+      const category = item[key];
+      acc[category] = (acc[category] || 0) + item.amount;
+      return acc;
+    },{});
+  };
+
+  const renderPieChart = (data,title,selectedMonth,handleMonthChange)=>{
+    const filteredData = filterByMonth(data.list,selectedMonth);
+    const categoryData = getCategoryData(filteredData,data.key);
+
+    const pieChartData = {
+      labels: Object.keys(categoryData).length > 0 ? Object.keys(categoryData) : ['No Data'],
+      datasets: [
+        {
+          data: Object.values(categoryData).length > 0 ? Object.values(categoryData) : [0],
+          backgroundColor: ['rgba(75, 192, 192, 0.6)', 'rgba(255, 99, 132, 0.6)', 'rgba(255, 206, 86, 0.6)'],
+        },
+      ],
+    };
+
+    return (
+      <Card className='mb-4 shadow-sm'>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Form.Group controlId={`${title.toLowerCase()}MonthSelect`} className="mb-3">
+            <Form.Label>Select Month</Form.Label>
+            <Form.Control as="select" value={selectedMonth} onChange={handleMonthChange}>
+              {['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].map((month, index) => (
+                <option key={index} value={index}>
+                  {month}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Pie data={pieChartData} />
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  const handleIncomeMonthChange = (event) =>{
+    setIncomeSelectedMonth(parseInt(event.target.value))
+  }
+
+  const handleExpenseMonthChange = (event) => {
+    setExpenseSelectedMonth(parseInt(event.target.value));
+  }
+
+
+
 
   return (
     <Container fluid>
@@ -45,77 +142,47 @@ export default function Dashboard() {
                     <h1 className="text-center mb-4">Dashboard</h1>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Row className="justify-content-center">
+                        <Col md={6}>
+                        <Card className="mb-4 shadow-sm">
+                            <Card.Body>
+                            <Card.Title>Income vs Expenses</Card.Title>
+                            <Bar data={chartData} />
+                            </Card.Body>
+                        </Card>
+                        </Col>
                         <Col md={4}>
                         <Card className="mb-4 shadow-sm">
                             <Card.Body>
-                            <Card.Title>Income</Card.Title>
-                            <h3>${totalIncome.toFixed(2)}</h3>
-                            <Table striped bordered hover responsive>
-                                <thead>
-                                <tr>
-                                    <th>Source</th>
-                                    <th>Amount</th>
-                                    <th>Date</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {incomeList.map((income) => (
-                                    <tr key={income._id}>
-                                    <td>{income.source}</td>
-                                    <td>${income.amount.toFixed(2)}</td>
-                                    <td>{new Date(income.date).toLocaleDateString()}</td>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </Table>
+                            <Card.Title>Total Net Work</Card.Title>
+                            <h3>${totalNetWork.toFixed(2)}</h3>
+                            <p>{totalNetWork >= 0 ? "You're within budget!" : "You're over budget!"}</p>
                             </Card.Body>
                         </Card>
                         </Col>
                     </Row>
                     <Row className="justify-content-center">
+                        <Col md={4}>
+                          {renderPieChart(
+                            {list: incomeList, key: 'type'},
+                            "Income by Category",
+                            incomeSelectedMonth,
+                            handleIncomeMonthChange
+                          )}
+              
+                        </Col>
+                        <Col md={4}>
+                          {renderPieChart(
+                            {list: expenseList, key: 'category'},
+                            "Expense by Category",
+                            expenseSelectedMonth,
+                            handleExpenseMonthChange
 
-                        <Col md={4}>
-                        <Card className="mb-4 shadow-sm">
-                            <Card.Body>
-                            <Card.Title>Expenses</Card.Title>
-                            <h3>${totalExpenses.toFixed(2)}</h3>
-                            <Table striped bordered hover responsive>
-                                <thead>
-                                <tr>
-                                    <th>Category</th>
-                                    <th>Amount</th>
-                                    <th>Date</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {expenseList.map((expense) => (
-                                    <tr key={expense._id}>
-                                    <td>{expense.category}</td>
-                                    <td>${expense.amount.toFixed(2)}</td>
-                                    <td>{new Date(expense.date).toLocaleDateString()}</td>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </Table>
-                            </Card.Body>
-                        </Card>
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-center">
-                        <Col md={4}>
-                        <Card className="mb-4 shadow-sm">
-                            <Card.Body>
-                            <Card.Title>Budget</Card.Title>
-                            <h3>${budget.toFixed(2)}</h3>
-                            <p>{budget >= 0 ? "You're within budget!" : "You're over budget!"}</p>
-                            </Card.Body>
-                        </Card>
+                          )}
                         </Col>
                     </Row>
                 </Row>
             </Col>
         </Row>
-        {/* <Sidebar/> */}
     </Container>
   );
 }
